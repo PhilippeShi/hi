9ed284d26f1d56c04d8a265aa51229d1127daf03
PhilippeShi <philippeshi17@gmail.com>
2024-02-03 17:58:20 -0500

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+      <h1>Hi Elly</h1>
+        <img src={'e0.jpeg'} className="App-logo e1" alt="logo"  
+        // style={{width: '100px', height: '100px'}}
+        />
+        <img src={'e1.jpeg'} className="App-logo e2" alt="logo"
+        // style={{width: '100px', height: '100px'}}
+        />
+        <img src={'e2.jpeg'} className="App-logo e3" alt="logo"
+        // style={{width: '100px', height: '100px'}}
+        />
+        <img src={'e3.jpeg'} className="App-logo e4" alt="logo"
+        // style={{width: '100px', height: '100px'}}
+        />
+        <img src={'e4.jpeg'} className="App-logo e5" alt="logo"
+        // style={{width: '100px', height: '100px'}}
+        />
+        <img src={'e5.jpeg'} className="App-logo e6" alt="logo"
+        // style={{width: '100px', height: '100px'}}
+        />
+        <img src={'e6.jpeg'} className="App-logo e7" alt="logo"
+        // style={{width: '100px', height: '100px'}}
+        />
+
+        {/* <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/philippe.shi"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Instagram
         </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/philippeshi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          className="App-link"
+          href="https://www.github.com/philippeshi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+
+        </a>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/philippe.shi.52"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </a> */}
+
+      
       </header>
+    
     </div>
   );
 }
